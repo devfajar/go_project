4b98aa657657f582007db07c0df1bf6192e925ab
devfajar <fajartrengginas@gmail.com>
2022-07-30 23:20:25 +0700

    add go-movies-crud func read data and delete data

--- src/go-movies-crud/main.go
+++ src/go-movies-crud/main.go
@@ -24,10 +24,29 @@ type Director struct {
 
 var movies []Movie
 
+// Read Function
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+// Delete Function
+func deleteMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 
-	movies = append(movies, Movie{ID: "1", Isbn: "438222", Title: "Movie One", Director: &Director})
+	movies = append(movies, Movie{ID: "1", Isbn: "438222", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "454555", Title: "Movie Two", Director: &Director{Firstname: "Steve", Lastname: "Smith"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
