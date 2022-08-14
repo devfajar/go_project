12b9df808a823f1a704fa160f2e822d4478c6700
devfajar <fajartrengginas@gmail.com>
2022-08-14 22:27:35 +0700

    add models

--- src/go-bookstore/pkg/models/book.go
+++ src/go-bookstore/pkg/models/book.go
@@ -1,8 +1,22 @@
 package models
 
+import (
+	"go-bookstore/pkg/config"
+
+	"github.com/devfajar/go-bookstore/pkg/config"
+)
+
 var db *gorm.db
 
 type Book struct {
 	gorm.model
-	Name string `gorm:""json:"name"`
+	Name        string `gorm:""json:"name"`
+	Author      string `gorm:""json:"author"`
+	Publication string `gorm:""json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
 }
