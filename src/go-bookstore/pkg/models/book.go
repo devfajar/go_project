f3a8753dc4f98f7c549db569867219948c91fb1b
devfajar <fajartrengginas@gmail.com>
2022-08-31 12:06:52 +0700

    add crud on models but controller not done

--- src/go-bookstore/pkg/models/book.go
+++ src/go-bookstore/pkg/models/book.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-	"go-bookstore/pkg/config"
+	"gorm.io/gorm"
 
 	"github.com/devfajar/go-bookstore/pkg/config"
 )
@@ -19,4 +19,28 @@ func init() {
 	config.Connect()
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db:=db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int64) {
+	var book Book
+	db.Where("ID=?", Id).Delete(&book)
+	return
 }
